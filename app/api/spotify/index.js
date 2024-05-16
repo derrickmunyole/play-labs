203e9601173454d36c1bb6ff9b004e7e9e5100bc
derrickmunyole <derrickjust@outlook.com>
2024-05-16 22:17:23 +0300

    Added create playlist route

--- app/api/spotify/index.js
+++ app/api/spotify/index.js
@@ -5,11 +5,13 @@ const router = express.Router();
 const spotifySearchRouter = require('./endpoints/search');
 const spotifyRecommendationsRouter = require('./endpoints/recommendations');
 const followedArtists = require('./endpoints/followedartists')
+const createPlaylist = require('./endpoints/createplaylist')
 
 // routes
 router.use('/spotify/search', spotifySearchRouter);
 router.use('/spotify/recommendations', spotifyRecommendationsRouter);
-router.use('/spotify/followedArtists', followedArtists)
+router.use('/spotify/followedArtists', followedArtists);
+router.use('/spotify/createPlaylist', createPlaylist);
 
 
 module.exports = router;
