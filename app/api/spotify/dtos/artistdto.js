d85b986d5e23490eec5a48265dfec16dbbe71e40
derrickmunyole <derrickjust@outlook.com>
2024-05-16 16:40:17 +0300

    Added artist DTO

---
+++ app/api/spotify/dtos/artistdto.js
@@ -0,0 +1,16 @@
+const ArtistDTO = {
+    externalUrls: {
+      spotify: "",
+    },
+    followers: {
+      total: 0,
+    },
+    genres: [],
+    href: "",
+    id: "",
+    images: [],
+    name: "",
+    popularity: 0,
+    type: "",
+    uri: "",
+  };
