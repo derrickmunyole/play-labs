caffc1f4425bf3f95b7db492e2faf4c025cd6f4b
derrickmunyole <derrickmunyole@outlook.com>
2024-06-01 10:38:08 +0300

    Added swagger documentation to the search endpoint

--- app/api/spotify/endpoints/search.js
+++ app/api/spotify/endpoints/search.js
@@ -5,7 +5,40 @@ const router = express.Router();
 const getSpotifyToken = require('../../../middleware/authentication');
 const retry = require('../../../utils/retry');
 
-
+/**
+ * @openapi
+ * /search:
+ *   get:
+ *     summary: Search for items on Spotify
+ *     description: Perform a search query on Spotify to find songs, albums, or artists.
+ *     parameters:
+ *       - in: query
+ *         name: q
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The search term to look for.
+ *       - in: query
+ *         name: type
+ *         schema:
+ *           type: string
+ *         enum: ['album', 'artist', 'track', 'playlist']
+ *         default: 'track'
+ *         description: The type of item to search for.
+ *     responses:
+ *       200:
+ *         description: A list of matching items.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/SearchResultItem'
+ *       400:
+ *         description: Bad request. Missing required parameter(s).
+ *       500:
+ *         description: Internal server error.
+ */
 router.get('/', getSpotifyToken, async (req, res) => {
   try {
     const query = req.query.q; 
